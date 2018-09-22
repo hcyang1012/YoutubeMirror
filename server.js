5a83783b2328c4b1c40a53e8c42d91ca59d732f1
Hwakyung Lee <heecheol.yang@outlook.com>
2018-09-22 20:13:57 +0900

    Reset file

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var videoapp = express();
+var remoteapp = express();
 
-app.get('/',function(req, res){
+var http1 = require('http').Server(videoapp);
+var io1 = require('socket.io')(http1);
+
+var videoid = "";
+
+videoapp.get('/',function(req, res){
+  res.sendFile(__dirname + '/youtube.html');
+});
+
+io1.on('connection', function(socket){
+});
+
+http1.listen(3000, function(){
+  console.log('server on!');
+});
+
+var http2 = require('http').Server(remoteapp);
+var io2 = require('socket.io')(http2);
+
+remoteapp.get('/',function(req, res){
   res.sendFile(__dirname + '/client.html');
 });
 
 var count=1;
-io.on('connection', function(socket){
-  console.log('user connected: ', socket.id);
-  var name = "user" + count++;
-  io.to(socket.id).emit('change name',name);
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected: ', socket.id);
-  });
-
-  socket.on('send message', function(name,text){
-    var msg = name + ' : ' + text;
-    console.log(msg);
-    io.emit('receive message', msg);
+io2.on('connection', function(socket){
+  socket.on('send message', function(text){
+    videoid = text;
+    console.log(videoid);
+    io1.emit('receive message', videoid);
   });
 });
 
-http.listen(3000, function(){
-  console.log('server on!');
-});
+http2.listen(8080, function(){
+  console.log('remote controller on!')
+})
